d994b6b861ccbc1bfa9d5d150b4467c921f203f5
Karun Agarwal <singhalkarun@gmail.com>
2024-08-06 19:10:00 +0530

    generate default values in sample.env if :- flag used in docker-compose*yaml

--- scripts/generate-combined-sample-env.sh
+++ scripts/generate-combined-sample-env.sh
@@ -12,11 +12,12 @@ OUTPUT_FILE="sample.env"
 # Function to extract environment variables from a docker-compose file
 extract_env_vars() {
   echo "Processing file: $1"
-  # Extract variables and their defaults, then filter out unwanted ones
-  grep -Eo '\${[^}]+}' "$1" | sed 's/[${}]//g' | \
+  # Extract variables and their defaults, then filter out unwanted ones 
+  grep -Eo '\${[^}]+}' "$1" | sed -e 's/[${}]//g' -e 's/:-\([^$]*\)/=\1/' | \
   grep -Eo '^[^:-]+' | sort -u  | \
   grep -Ev '(MEM_LIMIT|CPU_LIMIT|IMAGE_TAG|GITHUB_BRANCH)$' >> "$OUTPUT_FILE"
 }
+
 
 # Iterate over each base directory
 for BASE_DIRECTORY in "${BASE_DIRECTORIES[@]}"; do
@@ -34,8 +35,12 @@ done
 
 # Check if OUTPUT_FILE has content
 if [[ -s "$OUTPUT_FILE" ]]; then
-  # Remove duplicate entries
+  # Remove duplicate entries and sort
   sort -u "$OUTPUT_FILE" -o "$OUTPUT_FILE"
+  
+  # Sort lines with '=' to the top and lines without '=' to the bottom
+  (grep '=' "$OUTPUT_FILE"; grep -v '=' "$OUTPUT_FILE") | tee "$OUTPUT_FILE" > /dev/null
+  
   echo "Environment variables have been extracted to $OUTPUT_FILE"
 else
   echo "No environment variables found in the specified files."
